55a137e714a2b48d4ab149eb036cbae152bae7ab
Juliana Ferreira <julianafercavalcante@hotmail.com>
2022-10-19 15:31:09 -0300

    adições no js

--- JavaScript/temporadas.js
+++ JavaScript/temporadas.js
@@ -1,7 +1,7 @@
 const temporadas = [
     {
         id: 1,
-        nome: "temporada 1",
+        nome: "Temporada 1",
         resumo: "<p class=\"text-justify\">O início da série se passa em 1983, em Hawkins, Indiana. O jovem Will Byers (Noah Schnapp) desparece e seus amigos, Mike (Finn Wolfhard), Lucas (Caleb McLaughlin) e Dustin (Gaten Matarazzo) decidem procurá-lo com a sua família e a polícia da cidade. Com isso, eles se deparam com uma série de eventos sobrenaturais e é aí que eles começam a entender um pouco o Mundo Invertido.</p><p class=\"text-justify\">Nancy (Natalia Dyer), que é a irmã de Mike, Steve (Joe Keery), namorado de Nancy, e Jonathan (Charlie Heaton), irmão de Will, investigam por conta própria o que pode ter acontecido com o menino e Barb (Shannon Purser), a melhor amiga de Nancy que é morta por um Demogorgon no início da história.</p><p class=\"text-justify\">Nesse meio-tempo, conhecemos Eleven (Millie Bobby Brown), uma garota que tem superpoderes e não sabe muito sobre seu passado ou conviver em sociedade. No final, eles salvam Will, graças a um sacrifício de Eleven, mas os problemas dos protagonistas e a ligação do menino com o Mundo Invertido estão longe de acabarem.</p>",
         imagem: {
             print: "../imagens/Temporadas/Temporada-1.png",
@@ -50,6 +50,118 @@ const temporadas = [
                 titulo: 'De ponta-cabeça'
             }
         ] 
+    },
+    {
+        id: 2,
+        nome: "Temporada 2",
+        resumo: "<p class=\"text-justify\">Já na segunda temporada, os protagonistas tentam seguir em frente após tudo o que rolou no ano anterior. Só que uma nova ameaça aparece direto do Mundo Invertido para ameaçar os protagonistas. Além desse novo inimigo, também temos uma novata na escola: Max (Sadie Sink). Ela chega na cidade junto com seu irmão problemático, Billy (Dacre Montgomery).</p><p class=\"text-justify\">Enquanto isso, o detetive de Hawkins, Jim Hooper (David Harbour), decide criar Eleven, que sobreviveu aos acontecimentos do final da 1ª temporada, e é nesse ano que descobrimos mais sobre seu passado, sua mãe biológica, que ficou viva, mas teve sequelas após enfrentar uma terapia de eletrochoque, e a existência de uma irmã, a Eight/Kali (Linnea Berthelsen).</p><p class=\"text-justify\">Eleven fica escondida na cabana de Hopper e só o próprio Hopper sabe que ela está lá. Cansada dessa realidade, ela foge e, quando volta, aprende a verdade sobre sua história. Depois disso, seus amigos enfrentam um Demodog e Eleven entende que precisa fechar de vez o portal para o Mundo Invertido.</p><p class=\"text-justify\">No final desse ano, ela consegue destruir o Monstro das Sombras, que está conectado a Will e ameaçando a todos, e fica com Mike no baile da escola. Só que há uma vítima no meio disso tudo: Bob (Sean Astin), o novo interesse amoroso de Joyce. Também nessa temporada, Nancy termina com Steve e fica com Jonathan.</p>",
+        imagem: {
+            print: "../imagens/Temporadas/Temporada-2.png",
+            mobile: "../imagens/Temporadas/2temporada_500px.png",
+            poster: "../imagens/Posters/Temporada-2.jpg"
+        },
+        capitulos: [
+            {
+                temporada: '2',
+                numero: 'um',
+                titulo: 'Mad Max'
+            },
+            {
+                temporada: '2',
+                numero: 'dois',
+                titulo: 'Gostusura ou travessura, aberração'
+            },
+            {
+                temporada: '2',
+                numero: 'três',
+                titulo: 'O girino'
+            },
+            {
+                temporada: '2',
+                numero: 'quatro',
+                titulo: 'Will, o sábio'
+            },
+            {
+                temporada: '2',
+                numero: 'cinco',
+                titulo: 'Dig Dug'
+            },
+            {
+                temporada: '2',
+                numero: 'seis',
+                titulo: 'O espião'
+            },
+            {
+                temporada: '2',
+                numero: 'sete',
+                titulo: 'A irmã perdida'
+            },
+            {
+                temporada: '2',
+                numero: 'oito',
+                titulo: 'O Devorador de Mentes'
+            },
+            {
+                temporada: '2',
+                numero: 'nove',
+                titulo: 'O portal'
+            }        
+        ]
+    },
+    {
+        id: 3,
+        nome: "Temporada 3",
+        resumo: "<p class=\"text-justify\">Muita coisa importante acontece na 3ª parte de Stranger Things. Cientistas russos aparecem como vilões nesse ano da série, tentando abrir um portão para o Mundo Invertido embaixo do shopping de Hawkins, como revela Alexei (Alec Utgoff), um dos envolvidos nisso. Eleven descobre que o irmão de Max está possuído pelo Devorador de Mentes e, em uma futura batalha com o monstro, acaba sendo ferida e perdendo seus poderes.</p><p class=\"text-justify\">Essa temporada também aborda bastante de dramas adolescentes, com Eleven namorando Mike e Max com Lucas. No final da trama, Billy se sacrifica para proteger o grupo após a protagonista entrar na sua mente e Hopper tenta destruir a máquina usada pelos russos para manter o portal para a outra realidade aberto e é dado como morto. Eleven, então, vai morar com os Byers, que se mudam para a Califórnia.</p>",
+        imagem: {
+            print: "../imagens/Temporadas/Temporada-3.png",
+            mobile: "../imagens/Temporadas/3temporada_500px.png",
+            poster: "../imagens/Posters/Temporada-3.jpg"
+        },
+        capitulos: [
+            {
+                temporada: '3',
+                numero: 'um',
+                titulo: 'Está me ouvindo, Suzie?'
+            },
+            {
+                temporada: '3',
+                numero: 'dois',
+                titulo: 'O caso dos ratos'
+            },
+            {
+                temporada: '3',
+                numero: 'três',
+                titulo: 'A salva-vidas desaparecida'
+            },
+            {
+                temporada: '3',
+                numero: 'quatro',
+                titulo: 'A prova da sauna'
+            },
+            {
+                temporada: '3',
+                numero: 'cinco',
+                titulo: 'Os devorados'
+            },
+            {
+                temporada: '3',
+                numero: 'seis',
+                titulo: 'E pluribus unum'
+            },
+            {
+                temporada: '3',
+                numero: 'sete',
+                titulo: 'A mordida'
+            },
+            {
+                temporada: '3',
+                numero: 'oito',
+                titulo: 'A batalha de Startcourt'
+            }
+        ]
+    },
+    {
+
     }
 ]
 
