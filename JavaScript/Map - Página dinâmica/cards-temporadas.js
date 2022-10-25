5f064749cae8125a3805a6eb5098a3471f5cdaac
Juliana Ferreira <julianafercavalcante@hotmail.com>
2022-10-25 10:04:07 -0300

    alterações sucintas

--- JavaScript/Map - Página dinâmica/cards-temporadas.js
+++ JavaScript/Map - Página dinâmica/cards-temporadas.js
@@ -14,10 +14,10 @@ const temporadas = [
 ]
 
 function createTemporadas(temporadas) {
-    return `<a class="text-decoration-none" href="html/temporada${temporadas.temporada}.html">
+    return `<a class="text-decoration-none" href="html/temporadas.html?${temporadas.temporada}">
         <div id="temporada${temporadas.temporada}" class="div-temporadas d-flex flex-column justify-content-center align-items-center">
             <h1 class="h1-temporadas mb-0">${temporadas.temporada}ª</h1>
-            <h2 class="h2-temporadas mb-0">Temporadas</h2>
+            <h2 class="h2-temporadas mb-0">Temporada</h2>
         </div>
     </a>`
 }
