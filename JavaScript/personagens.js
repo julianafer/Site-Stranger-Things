692eaac15462785952b185858bc6693f2902ea1e
RicardoLuiz05 <ricardoluiz05.59@gmail.com>
2022-10-30 16:23:22 -0300

    correção do delete JS

--- JavaScript/personagens.js
+++ JavaScript/personagens.js
@@ -110,15 +110,19 @@ const personagens = [
 ]
 
 function createPerson(personagens) {
-    return `<div class="div-personagens ${personagens.id}">
+    return `
+<div id="div-personagens-${personagens.id}">
+    <div class="div-personagens ${personagens.id}">
     <a class="position-relative" href="${personagens.link}">
         <img class="img-personagens" src="${personagens.imagem}">
-        <button type='reset' class="lixeira p-0 border-0 position-absolute me-3" onclick="Delete(${personagens.id})">
+    </a>
+        <button type='reset' class="lixeira p-0 border-0 position-absolute me-3" onclick = 'Delete(${personagens.id})' >
             <i class="fa-sharp fa-solid fa-trash-can bg-transparent p-1 pb-2 ps-2"></i>
         </button>
         <h3 class="bg-red">${personagens.nome}</h3>
         <h4 class="bg-red">${personagens.ator}</h4>
-    </a>
+    
+    </div>
 </div>`
 }
 const PersonContent = personagens
@@ -130,12 +134,19 @@ const PersonContainer = document.querySelector('#cards');
 PersonContainer.innerHTML = PersonContent;
 
 function Delete(id){
-    const element = Array.from(personagens); 
-    delete element.splice(id-1, 1)
-    console.log(element)
-   // document.getElementById("#cards").innerHTML = element;
- }
+    const element = document.querySelector(`#div-personagens-${id}`)
+    element.remove()
+    element.stopPropagation
+
+}
 
 function Evento(){
     createPerson(personagens)
+    const PersonContent = personagens
+    .map((personagens) => createPerson(personagens))
+    .join('');
+
+    const PersonContainer = document.querySelector('#cards');
+
+    PersonContainer.innerHTML = PersonContent;
 }
