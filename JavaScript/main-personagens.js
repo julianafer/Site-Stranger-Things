d41ed2fd8db38761301ca47f1139eadd6a0f3753
RicardoLuiz05 <ricardoluiz05.59@gmail.com>
2022-10-30 01:31:27 -0300

    Alteração para JZON aos cards

--- JavaScript/main-personagens.js
+++ JavaScript/main-personagens.js
@@ -1,5 +1,8 @@
 const inputSearchPerson = document.querySelector('#exampleDataList.form-control');
 const CardsContainer = document.querySelector('#cards');
+const Card = document.querySelectorAll('.div-personagens')
+
+
 
 inputSearchPerson.addEventListener('input', event => {
     const inputValue = event.target.value.trim().toLowerCase()
@@ -10,7 +13,7 @@ inputSearchPerson.addEventListener('input', event => {
       .forEach(todo => {
           todo.classList.add('hidden')
       });
-  Cards
+    Cards
       .filter(todo => todo.textContent.toLowerCase().includes(inputValue))
       .forEach(todo => {
           todo.classList.remove('hidden')
