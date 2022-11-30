b0e69385f8b8380478fc1732bfb6d43b245bd1ea
Juliana Ferreira <julianafercavalcante@hotmail.com>
2022-11-30 17:03:13 -0300

    introdução do regex

--- JavaScript/main-personagens.js
+++ JavaScript/main-personagens.js
@@ -2,10 +2,12 @@ const inputSearchPerson = document.querySelector('#exampleDataList.form-control'
 const CardsContainer = document.querySelector('#cards');
 const Card = document.querySelectorAll('.div-personagens')
 
-
+const regex = /^[a-zA-Z]+\s?[a-zA-Z]+\s?[a-zA-z]*$/gm;
+// console.log(regex.test('millie bób brown'))
 
 inputSearchPerson.addEventListener('input', event => {
-    const inputValue = event.target.value.trim().toLowerCase()
+    const inputValue = regex.test(event.target.value.trim().toLowerCase())
+    console.log(inputValue)
     const Cards = Array.from(CardsContainer.children)
  
     Cards
