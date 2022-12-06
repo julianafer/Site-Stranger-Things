d626e095014fd487613922077c7bced4ed9603f6
Juliana Ferreira <julianafercavalcante@hotmail.com>
2022-12-06 08:46:56 -0300

    regex continuação parte 2

--- JavaScript/teste.js
+++ JavaScript/teste.js
@@ -1,22 +1,31 @@
-const inputSearchPerson = document.querySelector('#exampleDataList.form-control');
-const CardsContainer = document.querySelector('#cards');
-const Card = document.querySelectorAll('.div-personagens')
+async function search() {
+    const inputSearchPerson = document.querySelector('#exampleDataList.form-control');
+    const CardsContainer = document.querySelector('#cards');
+    const Card = document.querySelectorAll('.div-personagens')
 
-const regex = /^[a-zA-Z]+\s?[a-zA-Z]+\s?[a-zA-z]*$/gm;
-// regex para só aceitar no máximo 3 palavras, sem símbolos especiais
+    const regex = /^[a-zA-Z]+\s?[a-zA-Z]+\s?[a-zA-z]*$/gm;
+    // regex para só aceitar no máximo 3 palavras, sem símbolos especiais
 
-inputSearchPerson.addEventListener('input', event => {
-    const inputValue = event.target.value.trim().toLowerCase()
-    const Cards = Array.from(CardsContainer.children)
+    let h5 = document.getElementById('error')
 
-    Cards
-        .filter(todo => !todo.textContent.toLowerCase().includes(inputValue))
-        .forEach(todo => {
-            todo.classList.add('hidden')
-        });
-    Cards
-        .filter(todo => todo.textContent.toLowerCase().includes(inputValue))
-        .forEach(todo => {
-            todo.classList.remove('hidden')
+    if (regex.test(inputSearchPerson) === True) {
+        inputSearchPerson.addEventListener('input', event => {
+            const inputValue = event.target.value.trim().toLowerCase()
+            const Cards = Array.from(CardsContainer.children)
+        
+            Cards
+                .filter(todo => !todo.textContent.toLowerCase().includes(inputValue))
+                .forEach(todo => {
+                    todo.classList.add('hidden')
+                });
+            Cards
+              .filter(todo => todo.textContent.toLowerCase().includes(inputValue))
+              .forEach(todo => {
+                  todo.classList.remove('hidden')
+               })
         })
-})
+    }
+    else {
+        error.style.display = 'block'
+    }
+}
