231809e958c846d6d87a5cd03bc02f172ae37acb
Juliana Ferreira <julianafercavalcante@hotmail.com>
2022-12-06 17:00:53 -0300

    regex parte 3

--- JavaScript/teste.js
+++ JavaScript/teste.js
@@ -1,32 +1,9 @@
-async function search() {
-    const inputSearchPerson = document.querySelector('#exampleDataList.form-control');
-    const CardsContainer = document.querySelector('#cards');
-    const Card = document.querySelectorAll('.div-personagens')
-
-    const regex = /^[a-zA-Z]+\s?[a-zA-Z]+\s?[a-zA-z]*$/gm;
-    // regex para só aceitar no máximo 3 palavras, sem símbolos especiais
-
-    let h5 = document.getElementById('error')
-
-    inputSearchPerson.addEventListener('input', event => {
-        if (regex.test(inputSearchPerson)) {
-            const inputValue = event.target.value.trim().toLowerCase()
-            const Cards = Array.from(CardsContainer.children)
-            
-            Cards
-                .filter(todo => !todo.textContent.toLowerCase().includes(inputValue))
-                .forEach(todo => {
-                    todo.classList.add('hidden')
-                });
-            Cards
-                .filter(todo => todo.textContent.toLowerCase().includes(inputValue))
-                .forEach(todo => {
-                    todo.classList.remove('hidden')
-                })
-        }
-        else {
-            error.style.display = 'block'
-        }
-    }
-)}
-search()
+document.querySelector('input').oninvalid = function() {  
+    // remove mensagens de erro padrão
+    this.setCustomValidity("");
+    // faz a validação novamente
+    if (!this.validity.valid) {
+        // se estiver inválido, coloca a mensagem
+        this.setCustomValidity("Nome está inválido!");
+     }
+ };
